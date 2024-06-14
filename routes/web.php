3c1f40f7c8e3a392885663de79dd79674e062cd6
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2024-06-14 12:08:49 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\User\AdminController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+//user routes
+Route::middleware(['auth', 'userMiddleware'])->group(function(){
+    Route::get('dashboard', [UserController::class,'index'])->name('dashboard');
+});
+// admin routes
+Route::middleware(['auth', 'adminMiddleware'])->group(function(){
+    Route::get('/admin/dashboard', [Admincontroller::class,'index'])->name('admin.dashboard');
+});
